d59855afbb1347c9a4f31d267fe37c291805721b
denisgl1988 <denisgl1988@gmail.com>
2017-11-29 20:00:10 +0200

    122453

---
+++ src/conect.java
@@ -0,0 +1,14 @@
+/**
+ * Created by User on 29.11.2017.
+ */
+public class conect {
+
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 1;
+        int c = a + b;
+        System.out.println(c);
+
+    }
+}
